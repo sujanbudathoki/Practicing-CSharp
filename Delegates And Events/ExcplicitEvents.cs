30562ff393c89fb5f47c15289dc8cf501240171a
sujanbudathoki <sujanbudathoki24@gmail.com>
2021-11-13 09:05:07 +0545

    Added Explicit Events

---
+++ Delegates And Events/ExcplicitEvents.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Delegates_And_Events
+{
+    interface IBeforeEvents
+    {
+        public event EventHandler OnMyIntChanged;
+    }
+
+    interface IAfterEvents
+    {
+        public event EventHandler OnMyIntChanged;
+    }
+
+    public class SenderExcClass : IBeforeEvents, IAfterEvents
+    {
+        //creating 2 events
+        public event EventHandler BeforeIntChanged;
+        public event EventHandler AfterIntChanged;
+        //creating object for lock
+        object objecLock = new object();
+        private int myInt;
+        public int MyInt
+        {
+            get
+            {
+                return myInt;
+            }
+            set
+            {
+                BeforeIntChangedFunc();
+                myInt = value;
+                AfterIntChangedFunc();
+            }
+        }
+        event EventHandler IBeforeEvents.OnMyIntChanged
+        {
+            add
+            {
+                lock (objecLock)
+                {
+                    BeforeIntChanged += value;
+                }
+            }
+
+            remove
+            {
+                lock (objecLock)
+                {
+                    BeforeIntChanged -= value;
+                }
+            }
+        }
+
+        event EventHandler IAfterEvents.OnMyIntChanged
+        {
+            add
+            {
+                lock (objecLock)
+                {
+                    AfterIntChanged += value;
+                }
+            }
+
+            remove
+            {
+                lock (objecLock)
+                {
+                    AfterIntChanged += value;
+                }
+            }
+        }
+        public void BeforeIntChangedFunc()
+        {
+            if (BeforeIntChanged != null)
+            {
+                BeforeIntChanged(this, EventArgs.Empty);
+            }
+        }
+        public void AfterIntChangedFunc()
+        {
+            if (AfterIntChanged != null)
+            {
+                AfterIntChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+    //RecieverExcClass
+    public class RecieverExcClass
+    {
+        public void BeforeChanged(object sender,EventArgs e)
+        {
+            Console.WriteLine("Value is going to change.");
+        }
+
+        public void AfterChanged(object sender,EventArgs e)
+        {
+            Console.WriteLine("Value is changed now.");
+        }
+    }
+
+    //Program Class
+    public class ProgramExc
+    {
+        public  static void NewMain()
+        {
+            //sender Instance
+            SenderExcClass sender = new SenderExcClass();
+            //reciever Instance
+            RecieverExcClass reciever = new RecieverExcClass();
+            sender.MyInt = 2;
+            //subscribing function to beforeintchanged event.
+            sender.BeforeIntChanged += reciever.BeforeChanged;
+            sender.AfterIntChanged += reciever.AfterChanged;
+            //changing value
+            sender.MyInt = 3;
+        }
+    }
+
+}
